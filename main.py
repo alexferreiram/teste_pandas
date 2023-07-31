c3cf0ae4d921d9aeef2dda49584147ccdbac55aa
alexf <alexfmgt@gmail.com>
2023-07-30 23:45:48 -0300

    commit_30jul_23

--- main.py
+++ main.py
@@ -6,6 +6,27 @@
 
 import pandas as pd
 
-datframe_pd = pd.read_csv("arqcargaprev.txt", delim_whitespace=True)
-a = datframe_pd.describe()
-print(datframe_pd)
+# datframe_pd = pd.read_csv("coloncancer.txt", delim_whitespace=True)
+# a = datframe_pd.describe()
+# print(datframe_pd)
+
+demanda = dict()
+demanda['subsistema'] = list()
+demanda['semihora'] = list()
+demanda['MW'] = list()
+
+arq = open("arqcargaprev.txt", "r")
+s = arq.readline()
+
+
+while s[:5] != "TEMPO":
+    if s[:2] in (" 1", " 2", " 3", " 4"):
+        demanda['subsistema'].append(int(s[:3]))
+        demanda['semihora'].append(int(s[3:6]))
+        demanda['MW'].append(int(s[8:15]))
+    s = arq.readline()
+    # elif s[:51] =="--------------------------------------------------"
+
+df_cargaprev = pd.DataFrame(demanda)
+arq.close()
+print("teste")
